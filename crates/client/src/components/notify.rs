e174a92c5643b9f5e817a099361c3c4b1e008f83
theammir <the@ammir.dev>
2025-06-01 14:48:52 +0300

    feat(client): block-based notifications

--- crates/client/src/components/notify.rs
+++ crates/client/src/components/notify.rs
@@ -4,9 +4,9 @@ use color_eyre::eyre::Result;
 use ratatui::{
     Frame,
     layout::{Constraint, Layout, Rect},
-    style::{Style, Stylize},
+    style::Stylize,
     text::Line,
-    widgets::{Paragraph, Widget},
+    widgets::{Block, Clear, Paragraph, Widget},
 };
 
 use crate::{AppEvent, component::Component};
@@ -46,18 +46,36 @@ impl Component for Notification {
     fn render(&mut self, frame: &mut Frame, area: Rect, _is_focused: bool) {
         self.purge_expired();
 
-        let layout = Layout::horizontal([Constraint::Fill(1), Constraint::Percentage(20)]);
+        let layout = Layout::horizontal([Constraint::Fill(1), Constraint::Ratio(1, 3)]);
         let [_, notification_area] = layout.areas(area);
-        let notification_lines: Vec<Line> = self
-            .notifications
-            .iter()
-            .flat_map(|notif| notif.text.lines())
-            .map(|line| Line::styled(line, Style::new().black().on_green().bold()))
-            .collect();
-        Paragraph::new(notification_lines)
-            .right_aligned()
-            .wrap(ratatui::widgets::Wrap { trim: false })
-            .render(notification_area, frame.buffer_mut());
+        let notification_block = Block::bordered()
+            .border_type(ratatui::widgets::BorderType::Rounded)
+            .bold();
+
+        let mut offset_height: u16 = 0;
+        for notif in &self.notifications {
+            let inner_width = (notification_area.width - 2)
+                .min(notif.text.lines().map(|line| line.len()).max().unwrap_or(0) as u16);
+            let [_, inner_area_h] =
+                Layout::horizontal([Constraint::Fill(1), Constraint::Length(inner_width)])
+                    .areas(notification_area);
+            let paragraph = Paragraph::new(notif.text.lines().map(Line::from).collect::<Vec<_>>())
+                .wrap(ratatui::widgets::Wrap { trim: false })
+                .block(notification_block.clone());
+
+            // FIX: It doesn't work properly with manual linebreaks.
+            // And, according to ratatui #293, with a lot of other things.
+            let height = paragraph.line_count(inner_width) as u16;
+            let [_, inner_area] = Layout::vertical([
+                Constraint::Length(offset_height),
+                Constraint::Length(height),
+            ])
+            .areas(inner_area_h);
+            offset_height = offset_height.saturating_add(height);
+
+            frame.render_widget(Clear, inner_area);
+            paragraph.render(inner_area, frame.buffer_mut());
+        }
     }
 
     async fn handle_event(&mut self, event: AppEvent, _is_focused: bool) -> Result<bool> {
@@ -74,4 +92,3 @@ impl Component for Notification {
         })
     }
 }
-
